546749e9ec608af3607fb54af7ffdc9f93da07ee
jgutierrez00 <62234818+jgutierrez00@users.noreply.github.com>
2021-04-20 17:54:15 +0200

    Add files via upload

--- AsignacionGrupos/src/main/java/AsignacionGrupos/ModificarGrupoEJB.java
+++ AsignacionGrupos/src/main/java/AsignacionGrupos/ModificarGrupoEJB.java
@@ -1,5 +1,17 @@
 package AsignacionGrupos;
+import java.util.List;
+
 import Entidades.*;
+
 public class ModificarGrupoEJB {
+	
+	public List<Grupo> obtenerGrupoPorMatricula(Matricula m) {
+		Expediente e = m.getExpedientes_num_expedientes();
+		return e.getAlumno().getGrupos();
+	}
+	
+	public List<Matricula> obtenerMatriculaPorGrupo(Grupo g) {
+		return g.getAlumno().getExpedientes().get(0).getMatricula();
+	}
 
 }

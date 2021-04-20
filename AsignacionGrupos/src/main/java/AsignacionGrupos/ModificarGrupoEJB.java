04c8f6e387485042d211980951eb5c608bf63b6b
Alejandro Guzman Torres <kihacaoficial@gmail.com>
2021-04-20 09:48:38 -0700

    Nueva exception llamada NoExisteGrupoEnAlumno y "terminado" modificarGrupoAlumno y su EJB

--- AsignacionGrupos/src/main/java/AsignacionGrupos/ModificarGrupoEJB.java
+++ AsignacionGrupos/src/main/java/AsignacionGrupos/ModificarGrupoEJB.java
@@ -1,17 +1,35 @@
 package AsignacionGrupos;
+import java.util.Iterator;
 import java.util.List;
 
 import Entidades.*;
+import Exceptions.NoExisteGrupoEnAlumno;
 
 public class ModificarGrupoEJB {
 	
 	public List<Grupo> obtenerGrupoPorMatricula(Matricula m) {
 		Expediente e = m.getExpedientes_num_expedientes();
-		return e.getAlumno().getGrupos();
+		return e.getAlumno().getAlumno_Grupos();
 	}
 	
 	public List<Matricula> obtenerMatriculaPorGrupo(Grupo g) {
 		return g.getAlumno().getExpedientes().get(0).getMatricula();
 	}
-
+	
+	public void CambioGrupoAlumnos(List<Alumno> alum, Grupo antiguo, Grupo nuevo) throws NoExisteGrupoEnAlumno {
+		Iterator<Alumno> it = alum.iterator();
+	
+		while(it.hasNext()) {
+			Alumno al= it.next();
+			List<Grupo> grupos= al.getAlumno_Grupos();
+			
+			if(grupos.indexOf(antiguo)<0) {
+				throw new NoExisteGrupoEnAlumno();
+			}
+		
+			grupos.remove(antiguo);
+			grupos.add(nuevo);
+				
+		}
+	}
 }

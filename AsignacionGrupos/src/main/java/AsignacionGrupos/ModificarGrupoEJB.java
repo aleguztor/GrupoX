104b62c884751db868a5151a8c36d40aeeb77ccf
jgutierrez00 <62234818+jgutierrez00@users.noreply.github.com>
2021-04-21 16:30:32 +0200

    Add files via upload

--- AsignacionGrupos/src/main/java/AsignacionGrupos/ModificarGrupoEJB.java
+++ AsignacionGrupos/src/main/java/AsignacionGrupos/ModificarGrupoEJB.java
@@ -1,9 +1,15 @@
 package AsignacionGrupos;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
-import Entidades.*;
-import Exceptions.*;
+import Entidades.Alumno;
+import Entidades.Expediente;
+import Entidades.Grupo;
+import Entidades.Matricula;
+import Exceptions.AlumnoNoEncontradoException;
+import Exceptions.AsignacionGruposException;
+import Exceptions.NoExisteGrupoEnAlumno;
 
 public class ModificarGrupoEJB implements ModificarGrupoAlumno{
 	
@@ -12,8 +18,16 @@ public class ModificarGrupoEJB implements ModificarGrupoAlumno{
 		return e.getAlumno().getAlumno_Grupos();
 	}
 	
+	//Suponiedo que solo exista el expediente de informatica
 	public List<Matricula> obtenerMatriculaPorGrupo(Grupo g) {
-		return g.getAlumno().getExpedientes().get(0).getMatricula();
+		List<Alumno> alumnos = g.getAlumno();
+		Iterator<Alumno> it = alumnos.iterator();
+		List<Matricula> matriculas = new LinkedList<>();
+		while(it.hasNext()) {
+			Matricula m = it.next().getExpedientes().get(0).getMatricula().get(0);
+			matriculas.add(m);
+		}
+		return matriculas;
 	}
 	
 	public void CambioGrupoAlumnos(List<Alumno> alum, Grupo antiguo, Grupo nuevo) throws  NoExisteGrupoEnAlumno{

872c5dca2f9225a25da1b59585af9450a391ac77
AntonioBR99 <contactoevaristotempo@gmail.com>
2021-05-21 12:43:18 -0700

    Añadido un index.xhtml para navegacion

--- AsignacionGrupos-war/src/main/java/backingbeans/AlumnoController.java
+++ AsignacionGrupos-war/src/main/java/backingbeans/AlumnoController.java
@@ -5,22 +5,49 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import AsignacionGrupos.CrudEJB;
+import AsignacionGrupos.*;
 import Entidades.Alumno;
+import Exceptions.AlumnoNoEncontradoException;
 
-@Named
+@Named(value="alumnocon")
 @RequestScoped
 
 public class AlumnoController {
 	@Inject
-	private CrudEJB crud;
+	private CrudEJBLocal crud;
+	private String dni;
+	private Alumno alumno ;
 	
-	private Alumno alumno;
-	
-	public String doCreateAlumno() {
-		alumno = crud.buscarAlumnoPorDNI(dni);
-		
+	public String obtenterAlumno() {
+		try {
+			alumno = crud.buscarAlumnoPorDNI(dni);
+			if(alumno == null) {
+				return "failed";
+			}else {
+				return "success";
+			}
+		} catch (AlumnoNoEncontradoException e) {
+			e.printStackTrace();
+		}
+			return "failed";
 	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public Alumno getAlumno() {
+		return alumno;
+	}
+
+	public void setAlumno(Alumno alumno) {
+		this.alumno = alumno;
+	}
+	
 	
 	
 }

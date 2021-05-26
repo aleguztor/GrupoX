3c9106c369272dd7c44e166516702d3d9d4e2e9b
alumno <alumno@ubuntu>
2021-05-26 07:41:19 -0700

    Login hecho de secretria

---
+++ AsignacionGrupos-war/src/main/java/backingbeans/Login.java
@@ -0,0 +1,62 @@
+package backingbeans;
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+
+public class Login {
+	private String username;
+	private String password;
+	    
+	    
+	 public void login() {
+	        
+	        FacesContext context = FacesContext.getCurrentInstance();
+
+	        if(this.username.equals("secretaria") && this.password.equals("secretaria")){
+	            context.getExternalContext().getSessionMap().put("user", username);
+	            try {
+					context.getExternalContext().redirect("index.xhtml");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	        }
+	        else  {
+                    //Send an error message on Login Failure 
+	            context.addMessage(null, new FacesMessage("Authentication Failed. Check username or password."));
+
+	        } 
+	    }
+
+	    public void logout() {
+	    	FacesContext context = FacesContext.getCurrentInstance();
+	    	context.getExternalContext().invalidateSession();
+	        try {
+				context.getExternalContext().redirect("Login.xhtml");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    }
+	    
+	    
+		 public String getUsername() {
+				return username;
+			}
+
+			public void setUsername(String username) {
+				this.username = username;
+			}
+
+			public String getPassword() {
+				return password;
+			}
+
+			public void setPassword(String password) {
+				this.password = password;
+			}
+}

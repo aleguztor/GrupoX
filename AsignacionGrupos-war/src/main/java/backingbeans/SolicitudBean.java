7a3fe37f26b65399d86d6057985d82bb95886b42
Alejandro <kihacaoficial@uma.es>
2021-05-29 09:30:49 -0700

    HAY QUE HACER INSERT EN EL CRUD DE ENCUESTA, y AÑADIR NUEVOS ATRIBUTOS QUE ESTAN EN SOLCITIUD CAMBIO HORARIO

--- AsignacionGrupos-war/src/main/java/backingbeans/SolicitudBean.java
+++ AsignacionGrupos-war/src/main/java/backingbeans/SolicitudBean.java
@@ -1,5 +1,29 @@
 package backingbeans;
 
-public class SolicitudBean {
+import java.io.File;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import AsignacionGrupos.CrudEJBLocal;
+
+@Named
+@RequestScoped
+public class SolicitudBean {
+	private File archivo;
+	private String curso;
+	private String razones;
+	@Inject 
+	CrudEJBLocal crud;
+	
+	public void nuevoCurso(String c) {
+		
+	}
+	public void razones(String r) {
+		
+	}
+	public void uploadedFile(File f) {
+		
+	}
 }

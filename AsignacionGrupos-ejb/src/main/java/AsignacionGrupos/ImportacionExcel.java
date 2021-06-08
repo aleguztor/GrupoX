1cdf99cfe6cb7d3364ca706d03e8b17d4630fe97
AntonioBR99 <contactoevaristotempo@gmail.com>
2021-06-08 07:55:20 -0700

    import por ruta

--- AsignacionGrupos-ejb/src/main/java/AsignacionGrupos/ImportacionExcel.java
+++ AsignacionGrupos-ejb/src/main/java/AsignacionGrupos/ImportacionExcel.java
@@ -7,5 +7,5 @@ import Exceptions.ExcelNoEncontradoException;
 @Local
 public interface ImportacionExcel {
 
-	public void ImportarExcel() throws ExcelNoEncontradoException;
+	public void ImportarExcel(String ruta) throws ExcelNoEncontradoException;
 }

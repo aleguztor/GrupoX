e233c82b5085173b72c1c671775d254df8dc129c
AntonioBR99 <contactoevaristotempo@gmail.com>
2021-06-09 03:47:54 -0700

    por fin inserta desde excel

--- AsignacionGrupos-ejb/src/main/java/AsignacionGrupos/ImportacionExcelImpl.java
+++ AsignacionGrupos-ejb/src/main/java/AsignacionGrupos/ImportacionExcelImpl.java
@@ -36,13 +36,13 @@ public class ImportacionExcelImpl implements ImportacionExcel {
 			if (id == null)
 				id = 0L;
 			Workbook workbook = new XSSFWorkbook(inputStream);
-
+			boolean acaba = false;
 			Sheet firstSheet = workbook.getSheetAt(0); // miramos la hoja que vamos a usar
 			Iterator<Row> rowIterator = firstSheet.iterator(); // iterador de fila
 			Row next = rowIterator.next();
 			Iterator<Cell> cell = next.cellIterator();
 			String curso = null;
-			while (cell.hasNext()) {
+			while (cell.hasNext() && !acaba) {
 				Cell n = cell.next();
 				int columnIn = n.getColumnIndex();
 				if (columnIn == 1) {
@@ -53,7 +53,7 @@ public class ImportacionExcelImpl implements ImportacionExcel {
 			rowIterator.next();
 			rowIterator.next();
 			// saltamos las 4 primeras filas para obtener los datos
-			while (rowIterator.hasNext()) {
+			while (rowIterator.hasNext() &&!acaba) {
 				Row nextRow = rowIterator.next();
 				Alumno al = new Alumno();
 				Matricula mal = null;
@@ -63,7 +63,7 @@ public class ImportacionExcelImpl implements ImportacionExcel {
 				List<Expediente> expedientes;
 				List<Matricula> matriculas;
 				Iterator<Cell> cellIterator = nextRow.cellIterator(); // para movernos por la fial
-				while (cellIterator.hasNext()) {
+				while (cellIterator.hasNext() && !acaba) {
 					Cell nextCell = cellIterator.next();
 
 					int columnIndex = nextCell.getColumnIndex();
@@ -71,26 +71,51 @@ public class ImportacionExcelImpl implements ImportacionExcel {
 					switch (columnIndex) {
 					case 0: // DNI
 						String dni = nextCell.getStringCellValue();
+						if(dni==""){
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setDNI(dni);
 						LOG.info("mete dni " + dni);
 						break;
 					case 1: // NOMBRE
 						String nombre = nextCell.getStringCellValue();
+						if(nombre==""){
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setNombre(nombre);
 						LOG.info("mete nombre");
 						break;
 					case 2: // PRIMER APELLIDO
 						String apellido1 = nextCell.getStringCellValue();
+						if(apellido1==""){
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setApellido1(apellido1);
 						LOG.info("mete apellido1");
 						break;
 					case 3: // SEGUNDO APELLIDO
 						String apellido2 = nextCell.getStringCellValue();
+						if(apellido2==""){
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setApellido2(apellido2);
 						LOG.info("mete apellido2");
 						break;
 					case 4:// N EXPEDIENTE
 						String num_exp = nextCell.getStringCellValue();
+						if(num_exp == "") {
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						mpk.setExpedientes_num_expedientes(Long.parseLong(num_exp));
 						mpk.setCurso_academico(curso);
 						exp.setNum_Expediente(Long.parseLong(num_exp));
@@ -99,6 +124,11 @@ public class ImportacionExcelImpl implements ImportacionExcel {
 						break;
 					case 5: // N ARCHIVO
 						Integer n_archivo = (int) nextCell.getNumericCellValue();
+						if(n_archivo == null) {
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						mal = new Matricula(mpk, exp, 'A', d);
 						mal.setCurso_academico(curso);
 						mal.setNum_archivo(n_archivo);
@@ -111,42 +141,82 @@ public class ImportacionExcelImpl implements ImportacionExcel {
 						break;
 					case 7: // EMAIL_PERSONAL
 						String email_personal = nextCell.getStringCellValue();
+						if(email_personal == "") {
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setEmail_personal(email_personal);
 						LOG.info("mete email personal");
 						break;
 
 					case 8: // telefono
 						String telefono = nextCell.getStringCellValue();
+						if(telefono == ""){
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setTelefono(telefono);
 						LOG.info("mete telefono");
 						break;
 
 					case 9: // movil
 						String movil = nextCell.getStringCellValue();
+						if(movil == "") {
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setMovil(movil);
 						LOG.info("mete movil");
 						break;
 					case 10: // direccion
 						String direccion = nextCell.getStringCellValue();
+						if(direccion == "") {
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setDireccion_notificacion(direccion);
 						LOG.info("mete direccion");
 						break;
 					case 11: // localidad
 						String localidad = nextCell.getStringCellValue();
+						if(localidad == "") {
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setLocalidad_notificacion(localidad);
 						LOG.info("mete localidad");
 					case 12: // provincia
 						String provincia = nextCell.getStringCellValue();
+						if(provincia == ""){
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setProvincia_notificacion(provincia);
 						LOG.info("mete provincia");
 						break;
 					case 13: // cp
 						String cp = nextCell.getStringCellValue();
+						if(cp == "") {
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						al.setCP_notificacion(cp);
 						LOG.info("mete cp");
 						break;
 					case 14: // fecha_mat
 						String fecha = nextCell.getStringCellValue();
+						if(fecha == ""){
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						LOG.severe(fecha);
 						java.util.Date fecha1 = new SimpleDateFormat("dd/MM/yyyy hh:mm").parse(fecha);
 
@@ -158,12 +228,22 @@ public class ImportacionExcelImpl implements ImportacionExcel {
 						break;
 					case 16: // grupos_asignados
 						String list = nextCell.getStringCellValue();
+						if(list == ""){
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						mal.setListado_asignaturas(list);
 						LOG.info("mete grupos asignados" + list);
 						// NOSE RELLENAR ASIGNATURAS MATRICULA
 						break;
 					case 17: // nota_media
 						String nota_media = nextCell.getStringCellValue();
+						if(nota_media == "") {
+							LOG.info("faltan datos");
+							acaba = true;
+							continue;
+						}
 						LOG.info("mete nota media" + nota_media);
 						exp.setNota_Media_Provisional(Double.parseDouble(nota_media));
 
@@ -182,31 +262,35 @@ public class ImportacionExcelImpl implements ImportacionExcel {
 					case 23: // creditos tf
 						al.setId(id+1L);
 						id++;
-						/*
-						 * LOG.severe(al.toString()); exp.setAlumno(al); expedientes =
-						 * al.getExpedientes(); if (expedientes == null) { expedientes = new
-						 * LinkedList<Expediente>(); } expedientes.add(exp);
-						 * al.setExpedientes(expedientes); matriculas = exp.getMatricula();
-						 * 
-						 * matriculas.add(mal); exp.setMatricula(matriculas);
-						 * al.setExpedientes(expedientes); LOG.severe(exp.toString());
-						 * 
-						 * mal.setExpedientes_num_expedientes(exp); LOG.severe(exp.toString());
-						 */
+						
+						  LOG.severe(al.toString()); exp.setAlumno(al); expedientes =
+						  al.getExpedientes(); if (expedientes == null) { expedientes = new
+						  LinkedList<Expediente>(); } expedientes.add(exp);
+						  al.setExpedientes(expedientes); matriculas = exp.getMatricula();
+						  
+						  matriculas.add(mal); exp.setMatricula(matriculas);
+						  al.setExpedientes(expedientes); LOG.severe(exp.toString());
+						  
+						  mal.setExpedientes_num_expedientes(exp); LOG.severe(exp.toString());
+						 
 						LOG.severe(al.toString());
 						LOG.severe(mal.toString());
-						//crud.insertarExpediente(exp);
-						//crud.insertarMatricula(mal);
-						crud.insertarAlumno(al);
+						em.persist(em.merge(al));
+						LOG.info("ALUMNO INSERTADO");
 						
+						/*
+						 * em.persist(em.merge(exp)); LOG.info("EXPEDIENTE INSERTADO");
+						 * em.persist(em.merge(mal)); LOG.info("MATRICULAINSERTADO");
+						 */
 						break;
 					}
 
 				}
-
-				workbook.close();
+				
+				
 
 			}
+			workbook.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
